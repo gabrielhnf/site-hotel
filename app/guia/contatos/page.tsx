56f5d06ce31afccdbaaa5069ae1556cb0a257cbc
Gabriel Henrique <gabrielhenriquenf1@gmail.com>
2025-02-05 14:48:40 -0300

    Reformulated contacts page to be more responsive

--- app/guia/contatos/page.tsx
+++ app/guia/contatos/page.tsx
@@ -7,7 +7,6 @@ import close from "@/public/icons/closebrown.svg";
 
 import Image from "next/image";
 import { redirect, RedirectType } from "next/navigation";
-import Link from "next/link";
 
 const Contato = () => {
 
@@ -15,10 +14,10 @@ const Contato = () => {
         <div className="relative flex h-[calc(100vh_-_4rem)] sm:h-[calc(100vh_-_5rem)]">
 
             <div className="relative flex flex-col items-center justify-evenly
-                            w-full h-1/2 mb-auto mt-[25%] mx-2
-                            sm:w-2/3 sm:h-1/2 sm:mx-auto sm:my-auto
-                            md:w-2/3 md:h-2/3 md:mx-auto md:my-auto
-                            lg:w-2/3 lg:h-2/3 lg:mx-auto lg:my-auto
+                            w-full min-h-1/2 mb-auto mt-[25%] mx-2
+                            sm:w-2/3 sm:min-h-1/2 sm:mx-auto sm:my-auto
+                            md:w-2/3 md:min-h-2/3 md:mx-auto md:my-auto
+                            lg:w-2/3 lg:min-h-[60%] lg:mx-auto lg:my-auto
                             xl:w-1/3
                             px-2 bg-white shadow-2xl rounded-2xl"
                  style={{animation: "pulseIn 0.5s forwards"}}>
@@ -37,39 +36,32 @@ const Contato = () => {
                                md:text-xl">Selecione um dos meios de contato!</h1>
             
                 <div className="flex flex-row justify-evenly items-center
-                                w-[16rem] gap-x-5
+                                w-[80%] gap-x-5 h-fit flex-grow
                                 md:w-[20rem]
                                 lg:w-[24rem]" style={{aspectRatio: "2 / 1"}}>
 
-                    <Link href="https://wa.me/12981975865?text=Oi%2C queria tirar dúvidas sobre o apartamento 304 de itaguá." className="w-1/3 h-fit" target="__top">
-                        <button className="flex flex-col items-center justify-evenly
-                                           w-full h-fit
-                                           bg-white
-                                           transition-transform hover:scale-105 active:scale-100">
-                            <Image src={whatsapp} alt="Casa" className="h-[50%] w-fit rounded-2xl"/>
-                            <p className="max-w-[90%] text-ellipsis overflow-hidden text-[#754927]">Whatsapp</p>
-                        </button>
-                    </Link>
+                        <div className="w-1/4 h-fit flex-grow flex flex-col justify-start items-center overflow-hidden transition-transform hover:scale-105 cursor-pointer" onClick={() => {window.open("https://wa.me/12981975865?text=Oi%2C queria tirar dúvidas sobre o apartamento 304 de itaguá.", "__top")}}>
 
-                    <Link href="https://www.instagram.com/ap304itagua" className="w-1/3 h-fit" target="__top">
-                        <button className="flex flex-col items-center justify-evenly
-                                           w-full h-fit
-                                           bg-white
-                                           transition-transform hover:scale-105 active:scale-100">
-                            <Image src={instagram} alt="Casa" className="h-[50%] w-fit rounded-2xl"/>
-                            <p className="max-w-[90%] text-ellipsis overflow-hidden text-[#754927]">Instagram</p>
-                        </button>
-                    </Link>
+                                <Image src={whatsapp} alt="Whatsapp" className="h-[80%] w-fit aspect-square object-contain"/>
+                                <p className="line-clamp-1 text-md text-[#754927]">Whatsapp</p>
+                        
+                        </div>
 
-                    <Link href="https://www.airbnb.com.br/rooms/1237030016386778277" className="w-1/3 h-fit" target="__top">
-                        <button className="flex flex-col items-center justify-evenly
-                                           w-full h-fit
-                                           bg-white
-                                           transition-transform hover:scale-105 active:scale-100">
-                            <Image src={airbnb} alt="Casa" className="h-[50%] w-fit rounded-2xl"/>
-                            <p className="max-w-[90%] text-ellipsis overflow-hidden text-[#754927]">Airbnb</p>
-                        </button>
-                    </Link>
+                        <div className="w-1/4 h-fit flex-grow flex flex-col justify-start items-center overflow-hidden transition-transform hover:scale-105 cursor-pointer" onClick={() => {window.open("https://www.instagram.com/ap304itagua", "__top")}}>
+
+                                <Image src={instagram} alt="Whatsapp" className="w-full h-fit aspect-square object-contain"/>
+                                <p className="line-clamp-1 text-md text-[#754927]">Instagram</p>
+                        
+                        </div>
+
+                        <div className="w-1/4 h-fit flex-grow flex flex-col justify-start items-center overflow-hidden transition-transform hover:scale-105 cursor-pointer" onClick={() => {window.open("https://www.airbnb.com.br/rooms/1237030016386778277", "__top")}}>
+
+                                <Image src={airbnb} alt="Whatsapp" className="h-[80%] w-fit aspect-square object-contain"/>
+                                <p className="line-clamp-1 text-md text-[#754927]">Airbnb</p>
+                        
+                        </div>
+
+                    
 
                 </div>
 
